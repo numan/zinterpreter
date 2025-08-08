1ca9b090c4d12ea511e80cead7ec7ea78c92cc79
numan <numan@users.noreply.github.com>
2025-08-08 10:25:04 -0700

    chore: Add initial version of repl

--- src/root.zig
+++ src/root.zig
@@ -3,15 +3,5 @@
 //! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
-
-comptime {
-    _ = @import("lib/lexer.zig");
-}
-
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
+pub const lexer = @import("lib/lexer.zig");
+pub const repl = @import("repl/repl.zig");
