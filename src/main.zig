9d159564b02c35eee5d1a1af575dd4679fc94c31
numan <numan@users.noreply.github.com>
2025-08-08 10:29:32 -0700

    chore: Add a little more intro text

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,15 @@ const lib = @import("zinterpreter_lib");
 const repl = lib.repl;
 
 pub fn main() !void {
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Hello! This is the monkey programming language!\n", .{});
+    try stdout.print("This is a simple interpreter for the monkey programming language.\n", .{});
+    try stdout.print("Try to type in commands \n", .{});
+    try bw.flush();
+
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
